3188a5495c4962453b5488697fdb72225fa4c31f
lleastZ <kobzew07@gmail.com>
2022-03-22 14:17:32 +0500

    1.0.4

---
+++ route.php
@@ -0,0 +1,12 @@
+<?php
+
+use Controllers\MainController;
+use Controllers\UserController;
+
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/register', [UserController::class, 'register']);
+Route::post('/register', [UserController::class, 'registerPost']);
+
+Route::get('/login', [UserController::class, 'login']);
+Route::post('/login', [UserController::class, 'loginPost']);
