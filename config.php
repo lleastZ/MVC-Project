86777e376507ce1af2b6507656cf915ab5fc12a4
is42-kobzevng <kobzew07@gmail.com>
2022-03-23 10:33:49 +0500

    1.0.5

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = 'mysqlis.radiotech.su';
-const DB_USERNAME = 'admin_test';
-const DB_PASSWORD = 'NH1989Pr82';
-const DB_DATABASE = 'admin_test';
+const DB_USERNAME = 'is342_12_test';
+const DB_PASSWORD = '7tVxdgIZzI';//
+const DB_DATABASE = 'is342_12_testing';
 const DB_PORT = '3306'; # Default 3306
